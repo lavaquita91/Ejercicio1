3cc67ca7d3e2685053e97ea20890830ac3ae211c
lavaquita91 <christianmoises15@hotmail.com>
2024-08-20 13:58:03 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1/ChristianMora.cs
@@ -0,0 +1,25 @@
+﻿
+//url: https://dotnet.microsoft.com/es-es/languages/csharp
+
+//Sintaxis para crear Comentarios 
+// una linea
+
+/*Varias
+ * lineas*/
+
+///comentatios para xml
+
+string nombre = "Christian";
+const double Pi = 3.14;
+int cantidad = 0;
+double precio = 0;
+char inicial='a';
+bool estado = false;
+
+Console.WriteLine("Hola[C#]");
+Console.WriteLine(nombre);
+Console.WriteLine(Pi);
+Console.WriteLine(cantidad);
+Console.WriteLine(precio);
+Console.WriteLine(inicial);
+Console.WriteLine(estado);
